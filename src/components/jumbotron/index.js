0962bf931a17a32a4341343d27266b7d3795a8de
Deivide Meira <meiradeivide@gmail.com>
2022-01-19 17:40:16 -0300

    add jumbotron

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Inner } from './styles/jumbotron';
+
+const Jumbotron = () => {
+  return 
+  <Inner>
+      <p>Hello, world!</p>
+  </Inner>;
+};
+
+export default Jumbotron;
