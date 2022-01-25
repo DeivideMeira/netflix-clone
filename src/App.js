fc3270fe964bc97f78ba2852eedd23dc00f59014
Deivide Meira <meiradeivide@gmail.com>
2022-01-25 17:31:43 -0300

    add some changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ function App() {
     <Jumbotron.Container>
       {jumboData.map((item) =>(          
         <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Title>{item.title}</Jumbotron.Title>
-          <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
-          <Jumbotron.Image src={item.image} alt={item.alt}/>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
         </Jumbotron>
       ))}
     </Jumbotron.Container>
